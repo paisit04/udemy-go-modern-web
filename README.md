b623137952e59cbbc91186282cd359e6af8faf48
Paisit Jarunnamsiri <paisitj@MacBook-Pro.local>
2022-06-07 10:40:07 -0700

    Section 3: Building a Basic Web Application

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Building Modern Web Applications with Go (Golang)
 
 Ref: https://www.udemy.com/course/building-modern-web-applications-with-go
+
+## Bookings
+
+```sh
+go run cmd/web/main.go
+```
---
+++ bookings/cmd/web/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"myapp/pkg/config"
+	"myapp/pkg/handlers"
+	"myapp/pkg/render"
+	"net/http"
+)
+
+const portNumber = ":8080"
+
+// main is the main application function
+func main() {
+	var app config.AppConfig
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplate(&app)
+
+	http.HandleFunc("/", handlers.Repo.Home)
+	http.HandleFunc("/about", handlers.Repo.About)
+
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+	_ = http.ListenAndServe(portNumber, nil)
+}
